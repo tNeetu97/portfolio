cb5c04043275a5f9f32610a33c55a26a377b6df6
Neetu Verma <neetuv9718840@gmail.com>
2025-09-04 18:21:06 +0530

    data added

---
+++ index.js
@@ -0,0 +1,26 @@
+let express=require("express");
+let app=express();
+let port=8080;
+let path=require("path");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json());
+app.set("views",path.join(__dirname,"views"));
+app.set('view engine','ejs');
+app.listen(port,()=>{
+    console.log("Listening.....");
+})
+app.get("/portfolio",(req,res)=>{
+    res.render("home.ejs" );
+})
+app.get("/portfolio/simon",(req,res)=>{
+    res.render("simon.ejs");
+
+});
+app.post("/portfolio",(req,res)=>{
+    res.redirect("/portfolio");
+})
+app.get("/portfolio/Resume",(req,res)=>{
+    res.render("resume.ejs");
+}
+
+)
